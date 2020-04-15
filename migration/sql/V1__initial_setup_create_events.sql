99adb7b008d8e1fff6497a7fcba8c58628161355
Tony Oreglia <tony.oreglia@dashlane.com>
2020-04-15 09:19:12 +0100

    Create table breadcrums

--- migration/sql/V1__initial_setup_create_events.sql
+++ migration/sql/V1__initial_setup_create_events.sql
@@ -1,25 +1,8 @@
-CREATE TABLE IF NOT EXISTS numbers(
-  number        	  TEXT NOT NULL,
-  file_ref      	  UUID NOT NULL,
-  country_ioc_code 	TEXT NOT NULL,
-  PRIMARY KEY       (number, file_ref)
+CREATE TABLE IF NOT EXISTS breadcrumbs(
+  id          SERIAL PRIMARY KEY,           
+  data_type   CHAR(6) NOT NULL,
+  data_id	    INTEGER,
+  geog        GEOGRAPHY(POINT, 4326)
 );
 
-CREATE TABLE IF NOT EXISTS fixed_numbers (
-  original_number   TEXT NOT NULL,
-  changes           TEXT NOT NULL,
-  fixed_number      TEXT NOT NULL,
-  file_ref          UUID NOT NULL,
-  PRIMARY KEY       (original_number, file_ref)
-);
-
-CREATE TABLE IF NOT EXISTS rejected_numbers (
-  number        TEXT NOT NULL,
-  file_ref      UUID NOT NULL,
-  PRIMARY KEY   (number, file_ref)
-);
-
-
-GRANT ALL PRIVILEGES ON TABLE numbers TO breadcrumbs;
-GRANT ALL PRIVILEGES ON TABLE fixed_numbers TO breadcrumbs;
-GRANT ALL PRIVILEGES ON TABLE rejected_numbers TO breadcrumbs;
+GRANT ALL PRIVILEGES ON TABLE breadcrumbs TO breadcrumbs;
