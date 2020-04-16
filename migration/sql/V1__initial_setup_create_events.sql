1acb204d3b810846a1e75cf023905c2602600a9d
Tony Oreglia <tony.oreglia@dashlane.com>
2020-04-16 09:45:09 +0100

    Add foregin key constrain to breadcrumbs table

--- migration/sql/V1__initial_setup_create_events.sql
+++ migration/sql/V1__initial_setup_create_events.sql
@@ -1,8 +1,14 @@
+CREATE TABLE IF NOT EXISTS notes(
+  id          SERIAL PRIMARY KEY,
+  note        TEXT UNIQUE NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS breadcrumbs(
-  id          SERIAL PRIMARY KEY,           
+  id          SERIAL PRIMARY KEY,
   data_type   CHAR(6) NOT NULL,
-  data_id	    INTEGER,
+  data_id     INTEGER REFERENCES notes (id),
   geog        GEOGRAPHY(POINT, 4326)
 );
 
 GRANT ALL PRIVILEGES ON TABLE breadcrumbs TO breadcrumbs;
+GRANT ALL PRIVILEGES ON TABLE notes TO breadcrumbs;
