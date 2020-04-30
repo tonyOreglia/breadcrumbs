5a147203c5b141646ededc1ee26cedb1d4555b4f
Tony Oreglia <tony.oreglia@dashlane.com>
2020-04-30 08:32:43 +0100

    no longer needed migration file

--- migration/migrate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-
-docker run --rm -v /Users/toreglia/dev/breadcrumbs/migration/sql:/flyway/sql flyway/flyway -user="toreglia" -password="anthony" -url='jdbc:postgresql://localhost/breadcrumbs' -table=flyway_schema_history migrate info
